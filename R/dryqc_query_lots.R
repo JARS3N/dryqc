651e08e933b89d3f390f42c3ca9dd14be9e55dd4
JARS3N <JARS3N@users.noreply.github.com>
2017-12-20 12:32:09 -0500

    Update dryqc_query_lots.R

--- R/dryqc_query_lots.R
+++ R/dryqc_query_lots.R
@@ -3,7 +3,7 @@ library(RMySQL)
 mydb <- adminKraken::con_mysql()
 query_lots <- dbSendQuery(mydb, "call get_unique_dry_qc_lots();")
 dqc_lots <- dbFetch(query_lots)
-dbClearResult(a2)
+dbClearResult(query_lots)
 dbDisconnect(my_db)
 dqc_lots
 }
